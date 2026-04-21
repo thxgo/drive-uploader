4ed91b8f92df86935968fd64460d3eb4c8455498
thxgo <paduthiago02@gmail.com>
2026-04-20 23:13:06 -0300

    feat: add multiple file upload support

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ if not found:
     print(f"folder {today} created! - https://drive.google.com/drive/u/2/folders/{working_folder}")
  
 
-# create batch folder for each script run
+# create batch for each script run
 
 batch_id = service.files().create(
         body={
@@ -71,28 +71,30 @@ batch_id = service.files().create(
 
 # get file path from user and upload to batch
 
-file_path = input("file path: ") # TODO: add input validation 
-mime_type, _ = mimetypes.guess_type(file_path)
-file_name = os.path.basename(file_path) 
-media = MediaFileUpload(file_path, mimetype=mime_type)
+while True:
+    file_path = input("file path (or 'done' to finish): ") # TODO: add input validation 
+    if file_path == "done":
+        break
+    mime_type, _ = mimetypes.guess_type(file_path)
+    file_name = os.path.basename(file_path) 
+    media = MediaFileUpload(file_path, mimetype=mime_type)
+    file_id = service.files().create(
+            body={
+                "name": file_name,
+                "parents": [batch_id]
+            },
+            media_body=media,
+            fields="id",
+            supportsAllDrives=True
+    ).execute()["id"]
+    print(f"file {file_name} was sucessfuly uploaded in Google Drive! - https://drive.google.com/file/d/{file_id}/view")
 
-file_id = service.files().create(
+    # set file visibility to public
+
+    service.permissions().create(
+        fileId=file_id,
         body={
-            "name": file_name,
-            "parents": [batch_id]
-        },
-        media_body=media,
-        fields="id",
-        supportsAllDrives=True
-).execute()["id"]
-print(f"file {file_name} was sucessfuly uploaded in Google Drive! - https://drive.google.com/file/d/{file_id}/view")
-
-# set file visibility to public
-
-service.permissions().create(
-    fileId=file_id,
-    body={
-        "role": "reader",
-        "type": "anyone"
-    }
-).execute()
+            "role": "reader",
+            "type": "anyone"
+        }
+    ).execute()
