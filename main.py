4e1ccdca19813bd3a05a94c261261a7529a30887
thxgo <paduthiago02@gmail.com>
2026-04-21 21:51:09 -0300

    refactor: extract logic into functions

--- main.py
+++ main.py
@@ -14,88 +14,94 @@ OUTBOX_FOLDER_ID= "your_id_here"
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
 
-if os.path.exists(TOKEN_PATH):
-    creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
-else:     
-    flow = InstalledAppFlow.from_client_secrets_file(
-        CREDENTIALS_PATH,
-        scopes=SCOPES
-        )
-    creds = flow.run_local_server(port=0)
-    with open(TOKEN_PATH, "w") as token:
-        token.write(creds.to_json())       
-service = build("drive", "v3", credentials=creds)
-
-# list folders inside outbox
-
-folders = service.files().list(
-    q=f"'{OUTBOX_FOLDER_ID}' in parents",
-    fields="files(id, name)"
-).execute()["files"]
+def authenticate():
+    if os.path.exists(TOKEN_PATH):
+        creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
+    else:     
+        flow = InstalledAppFlow.from_client_secrets_file(
+            CREDENTIALS_PATH,
+            scopes=SCOPES
+            )
+        creds = flow.run_local_server(port=0)
+        with open(TOKEN_PATH, "w") as token:
+            token.write(creds.to_json())       
+    service = build("drive", "v3", credentials=creds)
+    return service
 
 # folder creating/checking
 
-today = datetime.now().strftime("%d-%m-%y")  
-now = datetime.now().strftime("%H-%M")
-
-found = False
-working_folder = None
-
-for folder in folders:
-    if folder["name"] == today:
-        found = True
-        working_folder = folder["id"] 
-        print(f"folder {today} exists! - https://drive.google.com/drive/u/2/folders/{working_folder}")
-
-if not found:
-    working_folder = service.files().create(
-        body={
-            "name": today,
-            "mimeType": "application/vnd.google-apps.folder",
-            "parents": [OUTBOX_FOLDER_ID]
-        },
-        fields="id"
+def get_daily_folder(service):
+    folders = service.files().list(
+        q=f"'{OUTBOX_FOLDER_ID}' in parents",
+        fields="files(id, name)"
+    ).execute()["files"]
+    today = datetime.now().strftime("%d-%m-%y")  
+    found = False
+    working_folder = None
+    for folder in folders:
+        if folder["name"] == today:
+            found = True
+            working_folder = folder["id"] 
+            print(f"folder {today} exists! - https://drive.google.com/drive/u/2/folders/{working_folder}")
+    if not found:
+        working_folder = service.files().create(
+            body={
+                "name": today,
+                "mimeType": "application/vnd.google-apps.folder",
+                "parents": [OUTBOX_FOLDER_ID]
+            },
+            fields="id"
         ).execute()["id"]
-    print(f"folder {today} created! - https://drive.google.com/drive/u/2/folders/{working_folder}")
- 
+        print(f"folder {today} created! - https://drive.google.com/drive/u/2/folders/{working_folder}")
+    return working_folder
 
 # create batch for each script run
 
-batch_id = service.files().create(
-        body={
-            "name": f"batch-{now}",
-            "mimeType": "application/vnd.google-apps.folder",
-            "parents": [working_folder]
-        },
-        fields="id"
-        ).execute()["id"]
+def create_batch(service, working_folder):
+    now = datetime.now().strftime("%H-%M")
+    batch_id = service.files().create(
+            body={
+                "name": f"batch-{now}",
+                "mimeType": "application/vnd.google-apps.folder",
+                "parents": [working_folder]
+            },
+            fields="id"
+    ).execute()["id"]
+    return batch_id
 
 # get file path from user and upload to batch
 
-while True:
-    file_path = input("file path (or 'done' to finish): ") # TODO: add input validation 
-    if file_path == "done":
-        break
-    mime_type, _ = mimetypes.guess_type(file_path)
-    file_name = os.path.basename(file_path) 
-    media = MediaFileUpload(file_path, mimetype=mime_type)
-    file_id = service.files().create(
+def upload_file(service, batch_id):
+    while True:
+        file_path = input("file path (or 'done' to finish): ") # TODO: add input validation 
+        if file_path == "done":
+            break
+        mime_type, _ = mimetypes.guess_type(file_path)
+        file_name = os.path.basename(file_path) 
+        media = MediaFileUpload(file_path, mimetype=mime_type)
+        file_id = service.files().create(
+                body={
+                    "name": file_name,
+                    "parents": [batch_id]
+                },
+                media_body=media,
+                fields="id",
+                supportsAllDrives=True
+        ).execute()["id"]
+        print(f"file {file_name} was sucessfuly uploaded in Google Drive! - https://drive.google.com/file/d/{file_id}/view")
+        # set file visibility to public
+        service.permissions().create(
+            fileId=file_id,
             body={
-                "name": file_name,
-                "parents": [batch_id]
-            },
-            media_body=media,
-            fields="id",
-            supportsAllDrives=True
-    ).execute()["id"]
-    print(f"file {file_name} was sucessfuly uploaded in Google Drive! - https://drive.google.com/file/d/{file_id}/view")
+                "role": "reader",
+                "type": "anyone"
+            }
+        ).execute()
 
-    # set file visibility to public
-
-    service.permissions().create(
-        fileId=file_id,
-        body={
-            "role": "reader",
-            "type": "anyone"
-        }
-    ).execute()
+def main():
+    service = authenticate()
+    working_folder = get_daily_folder(service)    
+    batch_id = create_batch(service, working_folder)
+    upload_file(service, batch_id)	
+if __name__ == "__main__":
+    main()
